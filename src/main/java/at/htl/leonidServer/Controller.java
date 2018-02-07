c0f669b2feaf0255b373b4512fc0141db717e9de
LukasRiedl <luki.riedliscool@gmail.com>
2018-02-07 12:13:27 +0100

    Outlined Something for testing

--- src/main/java/at/htl/leonidServer/Controller.java
+++ src/main/java/at/htl/leonidServer/Controller.java
@@ -23,35 +23,37 @@ public class Controller {
 
     public static String startWeb() throws IOException {
 
-        URL url = new URL("https://mese.webuntis.com/WebUntis/jsonrpc.do?school=htbla%20linz%20leonding");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-
-        String payload="{\"id\":\"LUKI\",\"method\":\"authenticate\"," +
-                "\"params\":{\"user\":\"in120083\",\"password\":\"luki05525252\"," +
-                "\"client\":\"Client\"},\"jsonrpc\":2.0}";
-
-        connection.setDoInput(true);
-        connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Accept", "application/json");
-        connection.setRequestProperty("Content-Type", "application/json");
-        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
-        writer.write(payload);
-        writer.close();
-        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuffer jsonString = new StringBuffer();
-        String line;
-        while ((line = br.readLine()) != null) {
-            jsonString.append(line);
-        }
-        br.close();
-        connection.disconnect();
-
-        System.out.println(jsonString.toString());
-
-       String x= jsonString.substring(52,84);
-
-        return x;
+//        URL url = new URL("https://mese.webuntis.com/WebUntis/jsonrpc.do?school=htbla%20linz%20leonding");
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//
+//
+//        String payload="{\"id\":\"LUKI\",\"method\":\"authenticate\"," +
+//                "\"params\":{\"user\":\"in120083\",\"password\":\"luki05525252\"," +
+//                "\"client\":\"Client\"},\"jsonrpc\":2.0}";
+//
+//        connection.setDoInput(true);
+//        connection.setDoOutput(true);
+//        connection.setRequestMethod("POST");
+//        connection.setRequestProperty("Accept", "application/json");
+//        connection.setRequestProperty("Content-Type", "application/json");
+//        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream(), "UTF-8");
+//        writer.write(payload);
+//        writer.close();
+//        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        StringBuffer jsonString = new StringBuffer();
+//        String line;
+//        while ((line = br.readLine()) != null) {
+//            jsonString.append(line);
+//        }
+//        br.close();
+//        connection.disconnect();
+//
+//        System.out.println(jsonString.toString());
+//
+//       String x= jsonString.substring(52,84);
+//
+//        return x;
+        //}
+        return null;
     }
 }
